c75cce8f12d96e3938a51ee4e257db9bf8c1c6cc
liewlucas <s10179187@connect.np.edu.sg>
2020-02-11 16:50:53 +0800

    Add project files.

---
+++ SEAssignmentConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEAssignmentConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
