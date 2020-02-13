22bfa48841b49e8e4acc2aa2eea4c5f3dac0f60c
Muhammad Thaqif Ghazali <s10182278@connect.np.edu.sg>
2020-02-14 01:29:29 +0800

    Chef done but need to add the order list class

---
+++ SEAssignmentConsole/Chef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEAssignmentConsole
+{
+    class Chef : Employee
+    {
+        public Chef(string Name, int ID, string Gender, string ContactNo, DateTime DateJoin, string Status) : base(Name, ID, Gender, ContactNo, DateJoin, Status)
+        { }
+
+        public void selectOrder(Order order)
+        {
+            order.status = "Preparing";
+            Console.WriteLine("Press enter when order is ready");
+            Console.ReadLine();
+            order.status = "Ready";
+            order.Readytime = DateTime.Now;
+            Console.WriteLine("Order is Completed");
+            Console.WriteLine("Exit?");
+            Console.ReadLine();
+        }
+    }
+}
